73cee8643b352e350ce5608c6f8d66434ffeaa28
SkyBlueHeat <br.aydin@hotmail.com>
2025-01-29 04:36:31 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Footer from "./layout/Footer";
 import HomePage from "./pages/HomePage";
-import ShopPage from "./pages/ShopPage"; // ShopPage'i ekliyoruz
-import Header from "./layout/Header"; // HomePage için Header
-import ShopHeader from "./layout/Shop/ShopHeader"; // ShopPage için ShopHeader
+import ShopPage from "./pages/ShopPage";  // ShopPage'i ekliyoruz
+
 
 function App() {
   return (
-    <Router basename="/">
+    <Router>
+      
       <Routes>
-        {/* HomePage için Header, ShopPage için ShopHeader */}
-        <Route path="/" element={<><Header /><HomePage /></>} />
-        <Route path="/shop" element={<><ShopHeader /><ShopPage /></>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} /> {/* ShopPage için route ekliyoruz */}
       </Routes>
       <Footer />
     </Router>
